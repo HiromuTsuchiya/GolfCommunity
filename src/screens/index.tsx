a92f5838a4e76359eca8b7ef99e0fad28708b59e
hiromutsuchiya <64008228+HiromuTsuchiya@users.noreply.github.com>
2024-04-11 16:47:03 +0900

    Learn more

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -90,6 +90,7 @@ function App(): JSX.Element {
           <Section title="Learn More">
             Read the docs to discover what to do next:
           </Section>
+          <Text>えええ</Text>
           <LearnMoreLinks />
         </View>
       </ScrollView>
